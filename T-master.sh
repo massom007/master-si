bc7ce69f509fe491cf122d6b8f7aefd0c082e626
massom007 <44805842+massom007@users.noreply.github.com>
2018-11-06 07:39:27 -0500

    Initial

---
+++ T-master.sh
@@ -0,0 +1,61 @@
+
+
+           echo  '    welcome To the Terminal Master Tool' | lolcat -a -d 50 -s 8
+             
+echo '      /---------------------------/-'| lolcat
+echo '     /___________________________/!\'| lolcat
+echo '     ||                         !! \'| lolcat
+echo '     !!      Termux Expert      !!  \'| lolcat -a -d 50
+echo '     !!                         !!  !'| lolcat
+echo '     !!  Tool-name[ T-master ]  !!  !'| lolcat
+echo '     !!                         !!  !'| lolcat
+echo '     !! coded by Masoom Arman   !!  !'| lolcat -a -d 50
+echo '     !!                         !!  !'| lolcat
+echo '     !!     enjoy and share     !!  /'| lolcat
+echo '     !!_________________________!! /'| lolcat
+echo '     !`_________________________\!/'| lolcat
+echo '       __\_________________/__|!_'| lolcat
+echo '     /_____\____________/_____!/'| lolcat
+echo '    ________________________'| lolcat
+echo '    /oooo  oooo  oooo  oooo /!'| lolcat
+echo '   /ooooooooooooooooooooooo/ /'| lolcat
+echo '  /ooooooooooooooooooooooo/ /'| lolcat
+echo ' /C=_____________________/_/' | lolcat
+       sleep 15
+
+            
+
+echo '_______________checking requiremenmts plz wait__________' | lolcat  -a -d 50 -s 9.0
+echo  '_______installing missing packjes______' | lolcat -a -d 50 -s 10.0 
+apt update
+ apt upgrade
+ pkg install python
+ pkg install python2 
+pip install --upgrade pip
+ pip2 install mechanize
+pip install gem
+gem install lolcat
+pkg install figlet
+pkg install cowsay
+
+                  echo '.-.-.-.-...Terminal is modifyng.-.-.-.-.-.' | lolcat -a -d 50 -s 15
+           
+rm -rf /data/data/com.termux/files/usr/etc/bash.bashrc
+mv /$HOME/Terminal-Master/Master/bash.bashrc /data/data/com.termux/files/usr/etc
+         
+
+
+
+            echo ' Setting up new terminal' | lolcat -a -d 50 -s 10
+                     sleep 5
+
+                
+
+
+
+            echo '     successfuly installed    ' | lolcat
+           
+
+rm -rf /data/data/com.termux/files/home/Terminal-Master
+ echo ' plz restart Termux and check ur terminal' | lolcat -a -d 50 -s 12
+sleep 5
